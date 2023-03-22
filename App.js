a8afac7748d7f8e89009b20c8ee03d01c6d9150f
Japji Multani <jpjsingh16@gmail.com>
2023-03-21 23:54:50 -0500

    Updated IntroView

--- App.js
+++ App.js
@@ -1,143 +1,139 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
-import {Feather} from '@expo/vector-icons';
-import {TitleView} from './MiddleViews';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { IntroView } from "./MiddleViews";
 
 export default function App() {
   return (
-    <View style={{position: 'relative', height: '100%', width:'100%'}}>
+    <View style={{ position: "relative", height: "100%", width: "100%" }}>
       <TopView></TopView>
       <MiddleView></MiddleView>
       <BottomView></BottomView>
-
     </View>
   );
 }
-
 
 const TopView = () => {
-  return(
+  return (
     <View style={topViewStyles.outerContainer}>
       <View style={topViewStyles.innerLeftContainer}>
-        <Image style={{height: 30, width: 120}} source={require('./Images/crunchyroll_icon.png')} resizeMode={'contain'}/>
+        <Image
+          style={{ height: 30, width: 120 }}
+          source={require("./Images/crunchyroll_icon.png")}
+          resizeMode={"contain"}
+        />
       </View>
-      
-      <View style={topViewStyles.innerRightContainer}> 
-        <Feather style={topViewStyles.innerRightIcon} name={"cast"} size={30}/>
-        <Feather style={topViewStyles.innerRightIcon} name={"search"} size={30}/>
+
+      <View style={topViewStyles.innerRightContainer}>
+        <Feather style={topViewStyles.innerRightIcon} name={"cast"} size={30} />
+        <Feather
+          style={topViewStyles.innerRightIcon}
+          name={"search"}
+          size={30}
+        />
       </View>
     </View>
   );
-}
+};
 
 const topViewStyles = StyleSheet.create({
   outerContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    position: 'absolute',
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    position: "absolute",
     left: 0,
-    width: '100%',
-    height: '13%',
+    width: "100%",
+    height: "13%",
     paddingLeft: 10,
     paddingRight: 10,
     paddingTop: 60,
-    backgroundColor: 'black',
-    zIndex: 1
+    backgroundColor: "black",
+    zIndex: 1,
   },
   innerLeftContainer: {
-    marginRight: 'auto',
-    
-
+    marginRight: "auto",
   },
   innerRightContainer: {
-    marginLeft: 'auto',
-    color: 'white',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    marginLeft: "auto",
+    color: "white",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   innerRightIcon: {
     margin: 4,
-    color: 'white'
-  }
+    color: "white",
+  },
 });
 
 const BottomView = () => {
-  
-  return(
+  return (
     <View style={bottomViewStyles.outerContainer}>
-      <BottomItem iconName= {'home'} description={'Home'}></BottomItem>
-      <BottomItem iconName= {'bookmark'} description={'My Lists'}></BottomItem>
-      <BottomItem iconName= {'grid'} description={'Browse'}></BottomItem>
-      <BottomItem iconName= {'tv'} description={'Simulcasts'}></BottomItem>
-      <BottomItem iconName= {'smile'} description={'Account'}></BottomItem>
+      <BottomItem iconName={"home"} description={"Home"}></BottomItem>
+      <BottomItem iconName={"bookmark"} description={"My Lists"}></BottomItem>
+      <BottomItem iconName={"grid"} description={"Browse"}></BottomItem>
+      <BottomItem iconName={"tv"} description={"Simulcasts"}></BottomItem>
+      <BottomItem iconName={"smile"} description={"Account"}></BottomItem>
     </View>
   );
-}
+};
 
 const BottomItem = (props) => {
-  return(
-    <View style={bottomViewStyles.bottomElem}> 
-      <Feather  name={props.iconName} size={25} color={'white'}/>  
+  return (
+    <View style={bottomViewStyles.bottomElem}>
+      <Feather name={props.iconName} size={25} color={"white"} />
       <Text style={bottomViewStyles.bottomElemText}> {props.description}</Text>
     </View>
-  )
-}
+  );
+};
 
 const bottomViewStyles = StyleSheet.create({
   outerContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    position: 'absolute',
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    position: "absolute",
     left: 0,
     bottom: 0,
-    backgroundColor: '#23252A',
-    width: '100%',
-    height: '10%',
+    backgroundColor: "#23252A",
+    width: "100%",
+    height: "10%",
     paddingLeft: 15,
     paddingRight: 15,
     zIndex: 1,
     paddingTop: 10,
-    alignItems: 'flex-start'
+    alignItems: "flex-start",
   },
   bottomElem: {
-    justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center',
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
     width: 55,
   },
-  bottomElemText:{fontSize: 9, marginTop: 5, color: 'white'}
-  
+  bottomElemText: { fontSize: 9, marginTop: 5, color: "white" },
 });
 
-const MiddleView = () =>
-{
-
-  const nums = Array.from({length: 100}, (_, k) => k);
-
+const MiddleView = () => {
+  const nums = Array.from({ length: 100 }, (_, k) => k);
+  const description =
+    "Around the end of the millennium, Viking, the mightiest but atrocious tibe, had been out breaking everywhere. Thorfinn, the son of the greatest warrior, lived his chil...";
+  const title = "VINLAND SAGA";
   console.log(nums);
-  return(
+  return (
     <ScrollView contentContainerStyle={middleViewStyles.outerContainer}>
-      <TitleView></TitleView>
-      {nums.map((v,i)=> <Text key={i}>{v}</Text>)}
+      <IntroView description={description} title={title}></IntroView>
+      {nums.map((v, i) => (
+        <Text key={i}>{v}</Text>
+      ))}
     </ScrollView>
   );
-}
+};
 
-const middleViewStyles = StyleSheet.create(
-  {
-    outerContainer: {
-      alignItems: 'center', 
-      width: '100%', 
-      display: 'flex'
-    }
-  }
-)
-
-
-
-
-
+const middleViewStyles = StyleSheet.create({
+  outerContainer: {
+    alignItems: "center",
+    width: "100%",
+    display: "flex",
+  },
+});
--- MiddleViews.js
+++ MiddleViews.js
@@ -1,46 +1,86 @@
-import {React} from 'react';
-import {ScrollView, Text, Image, StyleSheet, View, Button} from 'react-native';
+import { React } from "react";
+import {
+  ScrollView,
+  Text,
+  Image,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
 
-
-
-const IntroView = (props) =>
-{
-    return(
-        <View>
-            <Text>{props.title}</Text>
-            <Text>{props.description}</Text>
-        </View>
-    );
-}
+const IntroView = (props) => {
+  return (
+    <View style={introViewStyle.outerContainer}>
+      <ImageBackground
+        style={introViewStyle.backgroundImageContainer}
+        source={require("./Images/Vinland_Saga_Background.png")}
+        resizeMode={"cover"}
+      >
+        <TouchableOpacity style={introViewStyle.watchNowButton}>
+          <Feather name="play" size={20}></Feather>
+          <Text style={{ fontSize: 18 }}>WATCH NOW</Text>
+        </TouchableOpacity>
+        <Text style={{ color: "white" }}>{props.description}</Text>
+        <Text style={{ color: "white", fontSize: 24, marginBottom: 10 }}>
+          {props.title}
+        </Text>
+      </ImageBackground>
+    </View>
+  );
+};
 
 const introViewStyle = StyleSheet.create({
-
+  outerContainer: {
+    width: "100%",
+    height: 500,
+    display: "flex",
+    flexDirection: "column-reverse",
+  },
+  backgroundImageContainer: {
+    display: "flex",
+    flexDirection: "column-reverse",
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    height: "100%",
+  },
+  watchNowButton: {
+    backgroundColor: "#E47D3A",
+    color: "white",
+    width: 160,
+    height: 50,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 12,
+    marginTop: 10,
+  },
 });
 
-const ScrollingTitleView = (props) =>
-{
-    return(
-        <View>
-            <Text>{props.section}</Text>
-            <ScrollView horizontal={true}></ScrollView>
-        </View>
-    )
-}
+const ScrollingTitleView = (props) => {
+  return (
+    <View>
+      <Text>{props.section}</Text>
+      <ScrollView horizontal={true}></ScrollView>
+    </View>
+  );
+};
 
-const TitleView = (props) =>
-{
-    return(
+const TitleView = (props) => {
+  return (
+    <View>
+      <Image />
+      <View>
+        <Text></Text>
         <View>
-            <Image/>
-            <View>
-                <Text></Text>
-                <View>
-                    <Text></Text>
-                    <Button title={"val"}></Button>
-                </View>
-            </View>
+          <Text></Text>
+          <Button title={"val"}></Button>
         </View>
-    );
-}
+      </View>
+    </View>
+  );
+};
 
-export {IntroView, ScrollingTitleView, TitleView};
+export { IntroView, ScrollingTitleView, TitleView };
